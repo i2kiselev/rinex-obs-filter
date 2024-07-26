fdf1b7611cd187799c16d041061f6b22cb281eb2
i2kiselev <griggs1299@gmail.com>
2024-07-26 16:18:28 +0300

    feat: Add option of nullifying filtered entries

--- src/main/java/com/i2kiselev/rinexprocessor/service/RinexService.java
+++ src/main/java/com/i2kiselev/rinexprocessor/service/RinexService.java
@@ -15,11 +15,13 @@ import org.springframework.stereotype.Service;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 @Service
 public class RinexService {
 
+    public static final int NULL_SIGNAL = 0;
     private final RinexParser rinexParser;
 
     public RinexService(RinexParser rinexParser) {
@@ -27,16 +29,16 @@ public class RinexService {
     }
 
     public File completeRequest(String inputPath, String outputPath, TypeMatrix typeMatrix, FormatType formatType) {
-        RinexObservation rinexObservation = processObservation(inputPath, typeMatrix);
+        RinexObservation rinexObservation = processObservation(inputPath, typeMatrix, formatType);
         return FileUtils.writeOutputFile(outputPath, rinexObservation);
     }
 
-    RinexObservation processObservation(String filePath, TypeMatrix typeMatrix) {
+    RinexObservation processObservation(String filePath, TypeMatrix typeMatrix, FormatType format) {
         RinexObservation rinexObservation = rinexParser.parseObservation(filePath);
         List<ObservationDataSet> observationDataSets = rinexObservation.getObservationDataSets();
 
         List<ObservationDataSet> filteredBySystems = observationDataSets.stream().filter(x -> typeMatrix.getUsedSystems().contains(x.getSatellite().getSystem())).collect(Collectors.toList());
-        List<ObservationDataSet> updatedDataSets = filterObservationsByObs(filteredBySystems, typeMatrix);
+        List<ObservationDataSet> updatedDataSets = transformObservations(filteredBySystems, typeMatrix, format);
 
         RinexObservationHeader sourceHeader = rinexObservation.getHeader();
         RinexObservation updatedObservation = new RinexObservation();
@@ -48,8 +50,7 @@ public class RinexService {
         return updatedObservation;
     }
 
-
-    private List<ObservationDataSet> filterObservationsByObs(List<ObservationDataSet> observationDataSets, TypeMatrix typeMatrix) {
+    private List<ObservationDataSet> transformObservations(List<ObservationDataSet> observationDataSets, TypeMatrix typeMatrix, FormatType format) {
         List<ObservationDataSet> newObservationDataSets = new ArrayList<>();
         for (ObservationDataSet dataSet : observationDataSets) {
             ObservationDataSet updatedDataSet;
@@ -57,15 +58,44 @@ public class RinexService {
             SatelliteSystem currentSatSystem = currentSatellite.getSystem();
 
             List<ObservationData> observationData = dataSet.getObservationData();
-            List<ObservationData> filteredData = observationData.stream().filter(x -> typeMatrix.getUsedObs(currentSatSystem).contains(x.getObservationType().name())).collect(Collectors.toList());
+            List<ObservationData> filteredData;
+            if (format == FormatType.REMOVE) {
+                filteredData = filterObservationsByObs(observationData, typeMatrix, currentSatSystem);
+            } else {
+                filteredData = nullifyObservationsByObs(observationData, typeMatrix, currentSatSystem);
+            }
+
             if (filteredData.isEmpty()) {
                 continue;
             }
+            if (filteredData.stream().allMatch(x -> Double.isNaN(x.getValue()))) {
+                continue;
+            }
+
             updatedDataSet = new ObservationDataSet(dataSet.getSatellite(), dataSet.getDate(), dataSet.getEventFlag(), dataSet.getRcvrClkOffset(), filteredData);
             newObservationDataSets.add(updatedDataSet);
         }
         return newObservationDataSets;
     }
 
+    private List<ObservationData> filterObservationsByObs(List<ObservationData> observationData, TypeMatrix typeMatrix, SatelliteSystem satelliteSystem) {
+        return observationData.stream().filter(x -> typeMatrix.getUsedObs(satelliteSystem).contains(x.getObservationType().name())).collect(Collectors.toList());
+    }
+
+    private List<ObservationData> nullifyObservationsByObs(List<ObservationData> observationData, TypeMatrix typeMatrix, SatelliteSystem satelliteSystem) {
+        return observationData.stream()
+                .map(mapObservation(typeMatrix, satelliteSystem))
+                .collect(Collectors.toList());
+    }
+
+    private static Function<ObservationData, ObservationData> mapObservation(TypeMatrix typeMatrix, SatelliteSystem currentSatSystem) {
+        return x -> {
+            if (typeMatrix.getUsedObs(currentSatSystem).contains(x.getObservationType().name())) {
+                return x;
+            } else {
+                return new ObservationData(x.getObservationType(), Double.NaN, x.getLossOfLockIndicator(), NULL_SIGNAL);
+            }
+        };
+    }
 
 }
